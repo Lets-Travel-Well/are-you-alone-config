ee0c8c1808d1ddebe666807f4f5c470e754f73ea
marugy <gyulife7301@naver.com>
2023-06-15 23:34:56 +0900

    project start

---
+++ src/main/java/rualone/rualone/RualoneApplication.java
@@ -0,0 +1,13 @@
+package rualone.rualone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RualoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RualoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rualone/rualone/RualoneApplicationTests.java
@@ -0,0 +1,13 @@
+package rualone.rualone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RualoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
