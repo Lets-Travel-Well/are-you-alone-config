ff2d682a6d114b3e92e7ac40ea8b77c7d202417f
marugy <gyulife7301@naver.com>
2023-06-17 19:21:38 +0900

    build : change project

---
+++ src/main/java/rualone/areyoualoneconfig/AreYouAloneConfigApplication.java
@@ -0,0 +1,13 @@
+package rualone.areyoualoneconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreYouAloneConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AreYouAloneConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rualone/areyoualoneconfig/AreYouAloneConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package rualone.areyoualoneconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreYouAloneConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
